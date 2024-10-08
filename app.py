5301ac0f1038697d41d48c6c651914c206048494
DUYEN2809 <21520023@gm.uit.edu.vn>
2024-10-08 22:34:04 +0700

    Change da way aproching

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ from pathlib import Path
 import librosa
 import soundfile as sf
 from pydub import AudioSegment
-
-
+import speech_recognition as sr
+from transformers import pipeline
+device = torch.device('cuda')
 def convert_audio(save_path, output_file='D:\Audio\output.wav'):
     try:
         # Đọc file âm thanh gốc
@@ -26,21 +27,10 @@ def convert_audio(save_path, output_file='D:\Audio\output.wav'):
         print(f"File âm thanh đã được chuyển đổi thành: {output_file}")
     except:
         st.write(save_path)
-        sound = AudioSegment.from_file(save_path, format="m4a")
+        sound = AudioSegment.from_file(save_path, format="mp3")
         sound.export(output_file, format="wav")
-
-config = 'configs\\quartznet12x1_vi.yaml'
-encoder_checkpoint = 'models\\acoustic_model\\vietnamese\\JasperEncoder-STEP-289936.pt'
-decoder_checkpoint = 'models\\acoustic_model\\vietnamese\\JasperDecoderForCTC-STEP-289936.pt'
-lm_path = 'models/language_model/5-gram-lm.binary'
-vasr = VASR(
-    config_file=config,
-    encoder_checkpoint=encoder_checkpoint,
-    decoder_checkpoint=decoder_checkpoint,
-    lm_path=lm_path,
-    beam_width=100
-)
-#audio = audiorecorder("Click to record", "Click to stop recording")
+transcriber = pipeline("automatic-speech-recognition", model="vinai/PhoWhisper-small", device=0)
+keyword = "Vi"
 audio = st.file_uploader("Upload an audio file")
 if audio is not None:
     st.audio(audio.read())
@@ -49,6 +39,6 @@ if audio is not None:
     with open(save_path, mode='wb') as w:
         w.write(audio.getvalue())
     convert_audio(save_path=save_path)
-    wave, sr = librosa.load('D:\Audio\output.wav', sr=16000)
-    transcript = vasr.transcribe(wave)
-    st.write(transcript)
+    wave, ss = librosa.load('D:\Audio\output.wav', sr=16000)
+    output = transcriber(wave)['text']
+    st.write(output)
